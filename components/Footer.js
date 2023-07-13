28fdb9d25e2572849eb78ecb09b07e024c5cb28e
riki <r_karasawa@m.cajiya.co.jp>
2023-07-13 18:27:34 +0900

    ♻️ FooterとLinksをComponent化

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+export function Footer() {
+    return (
+        <>
+        <footer className={styles.footer}>
+            <span>Powered by Study React</span>
+        </footer>
+        </>
+    )
+}
